6ad7a6a9969e69cea8fd9958bf1ee1d1e1f3c13e
Rohit Ramesh <rkr@berkeley.edu>
2019-10-21 11:08:19 -0700

    Additional notes

--- app/Processor.hs
+++ app/Processor.hs
@@ -22,5 +22,17 @@ Goals:
     attempt to assign tags if possible
   build
     map of 2nd order references, w/ most cited getting stuff out
-  use sql-lite DB for content
+  use sql-lite DB for content?
+
+use grobid and cervine for parsing
+
+do we use a stack based model?
+
+What I want is a model where it grabs things from one pool and generates a
+potential match csv for the entire metadata pool
+
+well all the potential output csvs.
+
+
+
 -}
